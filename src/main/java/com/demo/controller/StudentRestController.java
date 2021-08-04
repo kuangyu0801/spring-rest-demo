793aa3186d597ffb93edf2fc696359d9b07e5a70
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-08-04 22:27:18 +0200

    Add Rest Student Handler

---
+++ src/main/java/com/demo/controller/StudentRestController.java
@@ -0,0 +1,23 @@
+package com.demo.controller;
+
+import com.demo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Poor", "Patel"));
+        students.add(new Student("Elon", "Musk"));
+        // spring will automatically handle data-binding
+        return students;
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -12,6 +12,8 @@
 </head>
 <body>
     <p> Home Page </p>
-    <a href="/test/hello">Hello</a>
+    <a href="${pageContext.request.contextPath}/test/hello">Hello</a>
+    <br><br>
+    <a href="${pageContext.request.contextPath}/api/students">Get Students</a>
 </body>
 </html>
