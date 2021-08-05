3f3388ce3b8fd15b78423667d2521e0d0bffbcfe
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-08-05 21:19:41 +0200

    Add endpoints for handle path variable

--- src/main/java/com/demo/controller/StudentRestController.java
+++ src/main/java/com/demo/controller/StudentRestController.java
@@ -2,9 +2,11 @@ package com.demo.controller;
 
 import com.demo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,12 +14,27 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    // this method will only execute once
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         students.add(new Student("Poor", "Patel"));
         students.add(new Student("Elon", "Musk"));
+        students.add(new Student("Bill", "Gates"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         // spring will automatically handle data-binding
         return students;
+    }
+
+    // define endpoint for "students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        // index into list
+        return students.get(studentId);
     }
 }
